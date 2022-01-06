97cd302733ab733a919e6235b5380fea82112430
p-drolima <pedro@pedrolima.me>
2022-01-06 18:42:29 +0100

    last edit

--- index.js
+++ index.js
@@ -3,300 +3,102 @@ const { PrismaClient } = require('@prisma/client')
 const axios = require('axios');
 const parseString = require('xml2js').parseString;
 const fs = require('fs');
-const { raw } = require('express');
+const { raw, response } = require('express');
+
+// const NC_API_KEY = '86eb8dd19891409991a421def2f365b3'
+// const NC_USERNAME = 'reachnames'
+// const NC_BASE  = 'api.sandbox.namecheap.com'
+
+const NC_API_KEY = '046dee541118428aa24984519c11b359'
+const NC_USERNAME = 'reachreserved2'
+const NC_BASE  = 'api.namecheap.com'
+
+let responseTime = {}
+
+let PROMISE_COUNT = 0
+
+const axiosClient = axios.create({
+  baseURL: `https://${NC_BASE}/`,
+  timeout: 100
+});
+
+axiosClient.interceptors.request.use(function (config) {
+  PROMISE_COUNT++
+  config.metadata = { startTime: new Date()}
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+axiosClient.interceptors.response.use(function (response) {
+
+  PROMISE_COUNT = Math.max(0 , PROMISE_COUNT - 1)
+ 
+  response.config.metadata.endTime = new Date()
+  response.duration = response.config.metadata.endTime - response.config.metadata.startTime
+
+  responseTime = { 
+                        
+    startTime: response.config.metadata.startTime,
+    endTime: response.config.metadata.endTime,
+    duration: response.duration
+
+  };
+
+  let fileName = JSON.stringify(responseTime.startTime)
+
+  responseTime = JSON.stringify(responseTime);
+  fs.writeFileSync(`logs/${fileName}-${PROMISE_COUNT}.json`, responseTime);
+
+  console.log('INTERCEPTOR:: ', responseTime)
+
+  console.log('DATA:: ', response.data)
+
+  return response.data;
+}, function (error) {
+  error.config.metadata.endTime = new Date();
+  error.duration = error.config.metadata.endTime - error.config.metadata.startTime;
+  return Promise.reject(error);
+});
 
 const env_ip = process.env.IP;
-
-let firstGoogle = false;
-let firstNamecheap = false;
-let firstWordpress = false;
 
 const prisma = new PrismaClient()
 const app = express()
 
+let domains, ncIP, maxCost, count = 0, rdapTime
+
+let endpoints = []
+
 app.use(express.json())
 
-let domains, rdapEvents, rdapDomainStatus, lastRegistrar, lastRegistrationTime, clashDate, premiumDomain = 'false', registrationRes, beforeRegTime, status, namecheapData, lastGoogleTime = '', firstGoogleTime = '', EAPfee, tier;
+async function init() {
 
-async function sendRegister(domain, cost, tier, ncIP, premiumPrice, registrationRes, firstGoogleTime, lastGoogleTime) {
-  
-  let date = new Date();
+  await data()
+  .then( response => {
 
-  if(tier === 'standard') {
+    domainName = response.domain
+    ncIP = response.nc_ip
+    endpoints = response.endpoints
+    rdap = response.rdap_time
+    //maxCost = response.max_cost
 
-    beforeRegTime = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();;
+    // let data = { 
+                        
+    //   domain_name: ,
 
-    if( status === 'ERROR' ) {
-
-      registrationRes = 'Namecheap check registration request had an error: ' + result.ApiResponse.Errors[0].Error[0]._;
-      
-      ncRegTime = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();;
-
-      if(firstGoogleTime !== '') {
-        await prisma.wp_options.update({
-          where: {
-            option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_registration_result'
-          },
-          data: {
-            option_value: 'Namecheap response: ' + registrationRes + '\n\n' + 'Google Request: ' + lastGoogleTime + '\n\n' +  'Before Registration attempt: ' + beforeRegTime + '\n\n'  + 'After Registration attempt: ' + ncRegTime
-          }
-        })
-      }
-
-      else if(lastGoogleTime !== '') {
-
-        await prisma.wp_options.update({
-          where: {
-            option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_registration_result_2'
-          },
-          data: {
-            option_value: 'Namecheap response: ' + registrationRes + '\n\n' + 'Google Request: ' + lastGoogleTime + '\n\n' +  'Before Registration attempt: ' + beforeRegTime + '\n\n'  + 'After Registration attempt: ' + ncRegTime
-          }
-        })
-
-      }
+    // };
     
-    } else if(status === 'OK') {
+    // googleResponseTime = JSON.stringify(googleResponseTime);
+    // fs.writeFileSync(`time-google.json`, googleResponseTime);
 
-      registrationRes = 'registered successfully'
-      
-      ncRegTime = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();;
+    run(domainName, ncIP, endpoints, rdap);
 
-      if(firstGoogleTime !== '') {
-        await prisma.wp_options.update({
-          where: {
-            option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_registration_result'
-          },
-          data: {
-            option_value: 'Namecheap response: ' + registrationRes + '\n\n' + 'Google Request: ' + lastGoogleTime + '\n\n' +  'Before Registration attempt: ' + beforeRegTime + '\n\n'  + 'After Registration attempt: ' + ncRegTime
-          }
-        })
-      }
-    
-
-      else if(lastGoogleTime !== '') {
-
-        await prisma.wp_options.update({
-          where: {
-            option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_registration_result_2'
-          },
-          data: {
-            option_value: 'Namecheap response: ' + registrationRes + '\n\n' + 'Google Request: ' + lastGoogleTime + '\n\n' +  'Before Registration attempt: ' + beforeRegTime + '\n\n'  + 'After Registration attempt: ' + ncRegTime
-          }
-        })
-
-      }
-    
-  }
-}
-  
-  else if(tier === 'premium' ) {
-
-      if( cost >= premiumPrice) {
-
-        beforeRegTime = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();;
-
-        if( status === 'ERROR' ) {
-              
-          registrationRes = 'Namecheap check registration request had an error: ' + result.ApiResponse.Errors[0].Error[0]._;
-          
-          ncRegTime = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();;
-          
-        //send email + block domain name in wp db
-
-        if(firstGoogleTime !== '') {
-          await prisma.wp_options.update({
-            where: {
-              option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_registration_result'
-            },
-            data: {
-              option_value: 'Namecheap response: ' + registrationRes + '\n\n' + 'Google Request: ' + lastGoogleTime + '\n\n' +  'Before Registration attempt: ' + beforeRegTime + '\n\n'  + 'After Registration attempt: ' + ncRegTime
-            }
-          })
-        }
-
-        else if(lastGoogleTime !== '') {
-
-          await prisma.wp_options.update({
-            where: {
-              option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_registration_result_2'
-            },
-            data: {
-              option_value: 'Namecheap response: ' + registrationRes + '\n\n' + 'Google Request: ' + lastGoogleTime + '\n\n' +  'Before Registration attempt: ' + beforeRegTime + '\n\n'  + 'After Registration attempt: ' + ncRegTime
-            }
-          })
-
-        }
-        
-      } else if( status === 'OK' ) {
-
-        if(firstGoogleTime !== '') {
-          await prisma.wp_options.update({
-            where: {
-              option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_registration_result'
-            },
-            data: {
-              option_value: 'Namecheap response: ' + registrationRes + '\n\n' + 'Google Request: ' + lastGoogleTime + '\n\n' +  'Before Registration attempt: ' + beforeRegTime + '\n\n'  + 'After Registration attempt: ' + ncRegTime
-            }
-          })
-        }
-
-        else if(lastGoogleTime !== '') {
-
-          await prisma.wp_options.update({
-            where: {
-              option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_registration_result_2'
-            },
-            data: {
-              option_value: 'Namecheap response: ' + registrationRes + '\n\n' + 'Google Request: ' + lastGoogleTime + '\n\n' +  'Before Registration attempt: ' + beforeRegTime + '\n\n'  + 'After Registration attempt: ' + ncRegTime
-            }
-          })
-
-        }
-
-      }
-
-      }
-      else {
-        registrationRes = 'Price higher than set cost'
-        
-        ncRegTime = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();;
-      }
-
-  }
+  });
 
 }
 
-async function callGoogle(domain, cost) {
-
-    console.log(domain);
-
-    let date = new Date();
-
-    let result, status;
-
-    let voidDate = date.getDate().toString() + date.getMonth().toString() + date.getFullYear().toString() + date.getHours().toString() + date.getMinutes().toString();
-
-    if(firstNamecheap === false || date.getMinutes() === 0 || date.getMinutes() === 20 || date.getMinutes() === 40 && voidDate !== clashDate ) {
-
-      clashDate = date.getDate().toString() + date.getMonth().toString() + date.getFullYear().toString() + date.getHours().toString() + date.getMinutes().toString();
-      firstNamecheap = true;
-      // call namecheap availability
-
-      status = result.ApiResponse.Status[0];
-
-      if(status === 'OK') {
-
-          premiumDomain = result.ApiResponse.CommandResponse[0].DomainCheckResult[0].IsPremiumName[0];
-
-          if(premiumDomain === 'true') {
-            premiumPrice = parseFloat(result.ApiResponse.CommandResponse[0].DomainCheckResult[0].PremiumRegistrationPrice[0]).toFixed(2)
-            premiumPrice = premiumPrice.toString();
-
-            namecheapData = { 
-              
-              domain: domain,
-              price: premiumPrice,
-
-            };
-            
-            namecheapData = JSON.stringify(namecheapData);
-            fs.writeFileSync(`${domain}.json`, namecheapData);
-
-          }
-
-      } 
-      
-      else if(status === 'ERROR') {
-
-          registrationRes = 'Namecheap check availability request had an error: ' + result.ApiResponse.Errors[0].Error[0]._;
-          
-      }
-
-    }
-
-    result = await axios.post('http://127.0.0.1:3000')
-    .then(async function (data) {
-        // handle success
-
-        console.log('Full: ', data.data.available);
-
-        result = data.data.available;
-
-        tier = data.data.tier;
-
-        await prisma.wp_options.update({
-            where: {
-              option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_ip_health'
-            },
-            data: {
-              option_value: 'Google request: Healthy'
-            }
-        })
-
-        if(data.data.available === true && firstGoogle === false) {
-
-            firstGoogle = true;
-            
-            let firstGoogleTime = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();
-
-            let rawdata = fs.readFileSync(`${domain}.json`);
-
-            premiumPrice = JSON.parse(rawdata).price;
-            
-            sendRegister(domain, cost, tier, ncIP, premiumPrice, registrationRes, firstGoogleTime, lastGoogleTime)
-
-            await prisma.wp_options.update({
-              where: {
-                option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_domain_health'
-              },
-              data: {
-                option_value: 'Namecheap response: ' + registrationRes
-              }
-            })
-
-        } 
-        
-        else if(data.data.available === true && firstGoogle === true) {
-
-            
-            let lastGoogleTime = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();
-            
-            sendRegister(domain, cost, tier, ncIP, premiumPrice, registrationRes, firstGoogleTime, lastGoogleTime)
-
-            await prisma.wp_options.update({
-              where: {
-                option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_domain_health'
-              },
-              data: {
-                option_value: 'Namecheap response: ' + registrationRes
-              }
-            })
-            
-
-        }
-        
-        else {
-            await prisma.wp_options.update({
-                where: {
-                  option_name: 'options_ipv6_0_add_ipv6s_' + env_ip + '_status_ip_domain_health'
-                },
-                data: {
-                  option_value: 'Domain available: Not Available'
-                }
-            })
-        }
-      })
-
-  }
-
-async function getDomains() {
-
-  let date = new Date();
-
-  if(firstWordpress === false || date.getMinutes() === 0 || date.getMinutes() === 20 || date.getMinutes() === 40) {
-    
-    firstWordpress = true
+async function data() {
 
     const options = await prisma.wp_options.findMany({
         where: {
@@ -306,44 +108,116 @@ async function getDomains() {
         }
     });
 
-    const ipNumber = options.filter(n => n.option_name === 'options_ipv6_0_add_ipv6s').map((v) => {
-      let ip_count = v.option_value
-      return ip_count;
-    }).toString();
+    domains = options.filter((option => option.option_name === 'options_ipv6_0_add_ipv6s_' + env_ip + '_add_domain_0_domain_name')).map((v) => {
+                domain = v.option_value
+                return domain;
+              }).toString();
 
     ncIP = options.filter(n => n.option_name === 'options_namecheap_ipv4_address').map((v) => {
-        let ncIP = v.option_value
-        return ncIP;
-      }).toString();
+              let ncIP = v.option_value
+              return ncIP;
+            }).toString();
 
-    domains = options.filter((option => option.option_name === 'options_ipv6_0_add_ipv6s_' + env_ip + '_add_domain_0_domain_name'));
-    serverRate = options.filter((option => option.option_name === 'options_ipv6_0_add_ipv6s_' + env_ip + '_ipv6_request_time_period')).map((r) => {
-      return r.option_value;
-    });
     maxCost = options.filter((option => option.option_name === 'options_ipv6_0_add_ipv6s_' + env_ip + '_add_domain_0_max_cost')).map((r) => {
         return r.option_value;
-      });
-
-  }
-    
-    domains.forEach(function(obj, index, collection) {
-
-        const domain = obj.option_value.toString();
-
-        setTimeout(function() {
-
-            (async () => {
-
-                callGoogle(domain, maxCost);
-                
-                getDomains();
-            
-            })();
-
-        }, parseInt(serverRate.toString()));
-
     });
+
+    const ENDPOINT = `xml.response?ApiUser=${NC_USERNAME}&ApiKey=${NC_API_KEY}&UserName=${NC_USERNAME}&Command=namecheap.domains.create&ClientIp=${ncIP}&DomainName=${domain}&Years=1&AuxBillingFirstName=Charlie&AuxBillingLastName=Coe&AuxBillingAddress1=7%20The%20Close%20,%20Muswell%20Avenue&AuxBillingStateProvince=London&AuxBillingPostalCode=N102ED&AuxBillingCountry=UK&AuxBillingPhone=+44.2084447848&AuxBillingEmailAddress=charlie@reachnames.com&AuxBillingOrganizationName=Dot31&AuxBillingCity=London&TechFirstName=Charlie&TechLastName=Coe&TechAddress1=7%20The%20Close%20,%20Muswell%20Avenue&TechStateProvince=London&TechPostalCode=N102ED&TechCountry=UK&TechPhone=+44.2084447848&TechEmailAddress=charlie@reachnames.com&TechOrganizationName=Dot31&TechCity=London&AdminFirstName=Charlie&AdminLastName=Coe&AdminAddress1=7%20The%20Close%20,%20Muswell%20Avenue&AdminStateProvince=London&AdminPostalCode=N102ED&AdminCountry=UK&AdminPhone=+44.2084447848&AdminEmailAddress=charlie@reachnames.com&AdminOrganizationName=Dot31&AdminCity=London&RegistrantFirstName=Charlie&RegistrantLastName=Coe&RegistrantAddress1=7%20The%20Close%20,%20Muswell%20Avenue&RegistrantStateProvince=London&RegistrantPostalCode=N102ED&RegistrantCountry=UK&RegistrantPhone=+44.2084447848&RegistrantEmailAddress=charlie@reachnames.com&RegistrantOrganizationName=Dot31&RegistrantCity=London&WithheldforPrivacy=yes&WGEnabled=no&GenerateAdminOrderRefId=False`
+
+    for( var i = 0; i < 1; i++) {
+      endpoints.push(ENDPOINT)
+    }
+
+    let rdap = await axios.get(`https://www.registry.google/rdap/domain/${domain}`)
+        .then( function (rdapData) {
+
+          console.log(rdapData.data.events)
+
+          rdapEvents = rdapData.data.events
+          rdapDomainStatus = rdapData.data.status
+          console.log(rdapDomainStatus)
+          if(rdapDomainStatus[0] === 'pending delete' || rdapDomainStatus[1] === 'pending delete') {
+            rdapEvents.map( (v) => {
+              switch(v.eventAction) {
+                case 'last changed':
+                  rdapTime = v.eventDate
+                  rdapTime = Date.parse(rdapTime)
+                  rdapTime += 1000 * 60 * 60 * 24 * 5;
+                  break;
+              }
+            });
+          }
+
+        })
+        .catch( function (rdapError) {
+
+          console.log('rdap ERROR: ', rdapError)
+
+        })
+
+        console.log(rdapTime)
+
+    let data = {
+      domain: domain,
+      nc_ip: ncIP,
+      endpoints: endpoints,
+      rdap_time: rdapTime
+    }
+
+    return data
 
 }
 
-getDomains();
+
+
+async function run(domain, ncIP, endpoints, rdapTime) {
+
+  //console.log(endpoints)
+
+  let date = Date.now()
+
+  if(! isNaN(rdapTime)) {
+    rdapOffset = rdapTime - 600
+  }
+  else {
+    rdapOffset = date - 1
+  }
+
+  console.log('Loop running:: ', date);
+
+  setTimeout(function() {
+
+    if(count < 1 && rdapOffset <= date) {
+
+      console.log('Time run start:: ', date);
+      console.log('RDAP Time:: ', rdapTime);
+
+      register(endpoints)
+
+      count++
+
+    } else {
+
+      run(domain, ncIP, endpoints, rdapTime)
+
+    }
+
+  }, 1);
+
+}
+
+async function register(endpoints) {
+
+  Promise.all(endpoints.map((endpoint) => axiosClient.get(endpoint, {timeout: 25000}))).then(response => {
+
+    axios.spread((response) => {
+      //console.log('SPREAD', response);
+    })
+
+  }).catch( error => {
+    console.log('AXIOS ERROR:: ', error)
+  });
+
+}
+
+init();
